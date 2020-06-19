3752388ccd77b75793cca697aff8c66acab7af39
Aaron Lohner <aaron.lohner@gmail.com>
2020-06-19 13:13:37 -0400

    test commit from vs code

--- project1/application.py
+++ project1/application.py
@@ -16,6 +16,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+#test for commit change
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
@@ -29,5 +30,6 @@ def index():
 def search():
     uname = request.form.get("username")
     pw = request.form.get("password")
+    print(pw)
     #verify that uname and pw are valid
     return render_template("search.html", name=uname)
