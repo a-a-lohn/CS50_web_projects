aede4a2ec1069a41026a42fa247a27336a99d06a
Aaron Lohner <aaron.lohner@gmail.com>
2020-06-21 17:44:11 -0400

    made create.py to create new db tables, app can add user to table

---
+++ project1/create.py
@@ -0,0 +1,19 @@
+# run this code to create tables from models.py in the db
+
+import os
+
+from flask import Flask, render_template, request
+# Import table definitions
+from models import *
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+def main():
+    db.create_all()
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
--- project1/models.py
+++ project1/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# this creates the db reference object
 db = SQLAlchemy()
 
 class User(db.Model):
@@ -7,3 +8,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
+
+class Book(db.Model):
+  __tablename__ = "book"
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String, nullable=True)
