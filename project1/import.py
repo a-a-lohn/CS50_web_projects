361a77d9cc269910cc4e2f96b964b1d048ee16fc
Aaron Lohner <aaron.lohner@gmail.com>
2020-06-24 11:20:38 -0400

    books imported

--- project1/import.py
+++ project1/import.py
@@ -4,6 +4,7 @@ import os
 
 from flask import Flask
 from models import db, Book
+import csv
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
@@ -11,13 +12,15 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 def main():
-    #LOOK AT BEER EXAMPLE TO SEE HOW THIS CAN BE DONE WITH GENERATORS
-    books = open("books.csv")
-    book.readline
-    #for row in books:
-    #    row = row.split(",")
-    #    db.session.add(Book(isbn=row[0], title=row[1], author=row[2] year=row[3][:3]))
-    #db.session.commit()
+
+    f = open("books.csv")
+    # books is a generator that, when next() is called, will split the row it yields after opening books.csv
+    books = (row for row in csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL))
+    # skip over the row of column names
+    next(books)
+    for row in books:
+        db.session.add(Book(isbn=row[0], title=row[1], author=row[2], year=row[3]))
+    db.session.commit()
 
 if __name__ == "__main__":
     # this is critical for the manipulation of the db within the app
